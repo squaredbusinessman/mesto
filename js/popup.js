fb8b0b82ad7bb34ff7af69cdbbd0d48418304cae
squaredbusinessman <burdaonelove@gmail.com>
2022-04-03 18:18:06 +0300

    hotfix: Разделяет код шаблона и рендеринга, создаёт каждой форме свой сабмит-хэндлер

--- js/popup.js
+++ js/popup.js
@@ -6,21 +6,22 @@ const profileEditPopup = document.querySelector('.popup_id_profile-edit');
 const addNewPostPopup = document.querySelector('.popup_id_new-post');
 const bigPicturePopup = document.querySelector('.popup_id_big-picture');
 
-// Необходимые элементы формы popup
-const formElements = document.querySelectorAll('.popup__form');
-
+// Необходимые элементы popup
 const editFormElement = profileEditPopup.querySelector('.popup__form');
 const nameInputElement = editFormElement.querySelector('.popup__input_type_name');
 const aboutInputElement = editFormElement.querySelector('.popup__input_type_about');
 const editPopupCloseButton = profileEditPopup.querySelector('.popup__close-btn');
+const editPopupSaveButton = profileEditPopup.querySelector('.popup__save-btn');
 
 const postFormElement = addNewPostPopup.querySelector('.popup__form');
 const picNameElement = postFormElement.querySelector('.popup__input_type_name');
 const picSrcElement = postFormElement.querySelector('.popup__input_type_about');
 const postPopupCloseButton = addNewPostPopup.querySelector('.popup__close-btn');
+const postPopupSaveButton = addNewPostPopup.querySelector('.popup__save-btn');
 
 const bigPicElement = bigPicturePopup.querySelector('.popup__img');
 const bigPicNameElement = bigPicturePopup.querySelector('.popup__name');
+const bigPicCloseButton = bigPicturePopup.querySelector('.popup__close-btn');
 
 // Необходимые элементы блока user
 const userSectionElement = document.querySelector('.user');
@@ -32,14 +33,6 @@ const addNewPostButton = userSectionElement.querySelector('.user__add-post-btn')
 // Необходимые элементы карточек
 const cardsContainer = document.querySelector('.cards');
 const cardTemplate = document.querySelector('#card-template').content;
-
-// Функция удаления карточек
-function deleteAllCards() {
-    const cards = cardsContainer.querySelectorAll('.card');
-    cards.forEach(function (card) {
-        card.remove();
-    })
-}
 
 // Функция кнопки лайка
 function likeButtonHandler(evt) {
@@ -72,6 +65,7 @@ function openBigPicPopup(evt) {
     bigPicElement.src = evt.target.src;
     bigPicElement.alt = evt.target.alt;
     bigPicNameElement.textContent = evt.target.alt;
+    bigPicCloseButton.addEventListener('click', closePopup);
     openPopup(bigPicturePopup);
 }
 
@@ -109,73 +103,63 @@ function closePopup() {
     document.removeEventListener('keyup', onEscKeyClosePopup);
 }
 
-// Функция отправки формы попапов
-function formSubmitHandler(evt) {
+// Функция отправки формы редактирования профиля
+function editFormSubmitHandler(evt) {
     evt.preventDefault();
-    switch (evt.target.name) {
-        case 'editProfileForm':
-            userNameElement.textContent = nameInputElement.value;
-            userAboutElement.textContent = aboutInputElement.value;
-            closePopup();
-            break;
-        case 'newPostForm':
-            const newCardsArr = [];
-            const cards = cardsContainer.querySelectorAll('.card');
-            const renderedCards = Array.from(cards, function(el) {
-                return {
-                    name: el.querySelector('.card__pic').alt,
-                    link: el.querySelector('.card__pic').src
-                }
-            });
+        userNameElement.textContent = nameInputElement.value;
+        userAboutElement.textContent = aboutInputElement.value;
+        closeProfileEditPopup();
+}
 
-            if (postFormElement.value !== '' && picSrcElement.value !== '') {
-                const newCard = {
-                    name: picNameElement.value,
-                    link: picSrcElement.value
-                }
-
-                newCardsArr.unshift(newCard);
-                deleteAllCards();
-                createCards(newCardsArr.concat(renderedCards));
-            }
-
-            closePopup();
-            break;
+// Функция отправки формы создания нового поста
+function newPostFormSubmitHandler(evt) {
+    evt.preventDefault();
+    const newCard = {
+        name: picNameElement.value,
+        link: picSrcElement.value,
     }
+
+   renderCard(newCard, cardsContainer);
+    closeNewPostPopup();
 }
 
-// Функция создания и добавления карточек из моковых данных
-function createCards(cardsArr) {
+// Функция создания карточки
+function getCardElement() {
+    // клонируем содержимое тега template
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    // необходимые элементы каждой карточки
+    const cardPicElement = cardElement.querySelector('.card__pic');
+    const removeButtonElement = cardElement.querySelector('.card__remove');
+    // обработчики событий на каждой карточке
+    cardElement.addEventListener('click', likeButtonHandler);
+    removeButtonElement.addEventListener('click', removeButtonHandler);
+    cardPicElement.addEventListener('click', (evt) => { openBigPicPopup(evt) });
 
-    cardsArr.forEach(function (card) {
-        // клонируем содержимое тега template
-        const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-
-        // необходимые элементы каждой карточки
-        const cardTitleElement = cardElement.querySelector('.card__title');
-        const removeButtonElement = cardElement.querySelector('.card__remove');
-        const cardPicElement = cardElement.querySelector('.card__pic');
-
-        // наполняем содержимым клонированный шаблон
-        cardPicElement.src = card.link;
-        cardPicElement.alt = card.name;
-        cardTitleElement.textContent = card.name;
-
-        // обработчики событий на каждой карточке
-        removeButtonElement.addEventListener('click', removeButtonHandler);
-        cardElement.addEventListener('click', likeButtonHandler);
-        cardPicElement.addEventListener('click', (evt) => { openBigPicPopup(evt) });
-
-        // отображаем на странице
-        cardsContainer.append(cardElement);
-    })
+    return cardElement;
 }
 
-createCards(initialCards);
+// Функция отображения карточки на странице
+function renderCard(card, container) {
+    const cardElement = getCardElement();
+    // необходимые элементы каждой карточки
+    const cardTitleElement = cardElement.querySelector('.card__title');
+    const cardPicElement = cardElement.querySelector('.card__pic');
+    // наполняем содержимым клонированный шаблон
+    cardPicElement.src = card.link;
+    cardPicElement.alt = card.name;
+    cardTitleElement.textContent = card.name;
 
+    container.prepend(cardElement);
+}
+
+function renderCards(cardsArr) {
+    cardsArr.forEach((card) => {
+        renderCard(card, cardsContainer);
+    });
+}
+
+renderCards(initialCards);
 nickEditButton.addEventListener('click', openProfileEditPopup);
 addNewPostButton.addEventListener('click', openNewPostPopup);
-
-formElements.forEach(function (el) {
-    el.addEventListener('submit', formSubmitHandler);
-});
+editPopupSaveButton.addEventListener('click', editFormSubmitHandler);
+postPopupSaveButton.addEventListener('click', newPostFormSubmitHandler);
