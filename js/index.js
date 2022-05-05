c6d27258914a487f42e7e53ef3e9ca32efd725d7
squaredbusinessman <burdaonelove@gmail.com>
2022-05-05 17:26:53 +0300

    fix: Чистит js-файлы проекта в соответствии с требованиями к коду Я.Практикума к ПР№7

--- js/index.js
+++ js/index.js
@@ -1,7 +1,41 @@
 import Card from './Card.js';
 import FormValidator from './FormValidator.js';
-import {initialCards} from './data-cards.js';
-import {validationConfig} from './validate.js';
+
+// Моковые данные
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+// Конфиги
+const validationConfig = {
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__save-btn',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__input-error_visible',
+};
 
 // Константы
 const ESC_KEY = 'Escape';
