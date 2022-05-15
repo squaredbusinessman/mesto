e1eabe53d1419c3214d16560dfa856c18c30db61
squaredbusinessman <burdaonelove@gmail.com>
2022-05-15 17:25:14 +0300

    feat: Создаёт класс-слой Section

---
+++ js/Section.js
@@ -0,0 +1,15 @@
+export default class Section {
+    constructor({ itemsArray, rendererFunction }, containerSelector) {
+        this._initialArray = itemsArray; // Массив данных, которые необходимо отрисовать при инициализации класса
+        this._renderer = rendererFunction; // Функция отвечающая за создание и отрисовку данных
+        this._container = containerSelector; // Селектор контейнера для добавления созданных элементов
+    }
+
+    renderItems() {
+        this._initialArray.forEach(item => this._renderer(item));
+    }
+
+    addItem(element) {
+        this._container.append(element);
+    }
+}
