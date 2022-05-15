cfbfcfe1c6a4e04abbef40930df7dc0fbba47517
squaredbusinessman <burdaonelove@gmail.com>
2022-05-15 18:25:23 +0300

    feat: Создаёт класс PopupWithImage, наследуемый от класса Popup

---
+++ js/PopupWithImage.js
@@ -0,0 +1,17 @@
+import Popup from "./Popup";
+
+export default class PopupWithImage extends Popup {
+    constructor(popupSelector) {
+        super(popupSelector); // наследуем от родительского класса
+        this._picElement = this._popup.querySelector('.popup__img');
+        this._picTitle = this._popup.querySelector('.popup__name');
+    }
+
+    open(evt) { // Сделали перегрузку метода - расширили функциональность метода родительского класса
+        this._picElement.src = evt.target.src;
+        this._picElement.alt = evt.target.alt;
+        this._picTitle.textContent = evt.target.alt;
+
+        super.open(); // вызываем родительский метод
+    }
+}
