09ee9aae4d29a419235bff46b39013f52fb8c016
squaredbusinessman <burdaonelove@gmail.com>
2022-05-16 10:09:01 +0300

    feat: Настраивает обработку изображений и шрифтов

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,13 @@ module.exports = {
                 use: 'babel-loader',
                 // исключает папку node_modules, файлы в ней обрабатывать не нужно
                 exclude: '/node_modules/'
-            }
+            },
+            // добавили правило для обработки файлов
+            {
+                // регулярное выражение, которое ищет все файлы с такими расширениями
+                test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+                type: 'asset/resource'
+            },
         ]
     },
     plugins: [
