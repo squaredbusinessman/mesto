40c2b93dfed2546056acf6b39ff057bfcfed130a
squaredbusinessman <burdaonelove@gmail.com>
2022-05-16 10:34:18 +0300

    fix: Добавляет обработчик закрытия по клику на Х в метд setEventListeners класса popup

--- src/js/Popup.js
+++ src/js/Popup.js
@@ -1,6 +1,7 @@
 export default class Popup {
     constructor(popupSelector) {
         this._popup = document.querySelector(popupSelector);
+        this._buttonClose = this._popup.querySelector('.popup__close-btn')
     }
 
     _handleEscClose(evt) { // приватный метод закрытия попапа нажатием Esc
@@ -30,9 +31,12 @@ export default class Popup {
         if (this._popup.classList.contains('popup_visible')) {
             this._popup.addEventListener('click', evt => this._handleOverlayClose(evt));
             document.addEventListener('keyup', evt => this._handleEscClose(evt));
+            this._buttonClose.addEventListener('click', () => this.close());
+
         } else {
             this._popup.removeEventListener('click', evt => this._handleOverlayClose(evt));
             document.removeEventListener('keyup', evt => this._handleEscClose(evt));
+            this._buttonClose.removeEventListener('click', () => this.close());
         }
     }
 }
